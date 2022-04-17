f7cb41abcf3f055bef062cbe82052db70b45f809
jaturapjat2543 <6206021611095@fitm.kmutnb.ac.th>
2022-04-17 16:48:09 +0700

    Bomz

--- app/src/main/java/com/example/myapplication/catagory.java
+++ app/src/main/java/com/example/myapplication/catagory.java
@@ -1,30 +1,39 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.Button;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-    TextView btnCata ;
+import java.util.List;
+
+public class catagory extends AppCompatActivity implements View.OnClickListener {
+   TextView btnChange;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_catagory);
 
-        btnCata = findViewById(R.id.btnCata);
-
+        btnChange = findViewById(R.id.btnChange);
     }
     @Override
     public void onClick(View v) {
         int id = v.getId();
-        if(R.id.btnCata == id){
+
+        if(R.id.btnChange == id){
             Intent textSwitcherActivity = new Intent(this, catagory.class);
             startActivity(textSwitcherActivity);
         }
 
     }
+
 }
+
